64559396ebb9283d766c328b0be2a0357422cd04
alexey <a_voronin@list.ru>
2022-03-26 22:35:24 +0300

    test commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from facebook_scraper import get_posts
 from facebook_scraper import *
+from staty import *
 from resources import creds  # put your login info in this file and variable locally
 
-
+#merge1
+print(1)
 for post in get_posts(group='1543813289196226/', credentials=creds, pages=1):
     print(post['text'][:50])
 
