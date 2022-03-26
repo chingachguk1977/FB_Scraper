e59d90f418b777cb864be1b8338be212261a0e60
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-03-26 22:08:25 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from facebook_scraper import get_posts
 from facebook_scraper import *
-from resources import *
+from resources import creds  # put your login info in this file and variable locally
 
-
-creds = ('', '')
 
 for post in get_posts(group='1543813289196226/', credentials=creds, pages=1):
     print(post['text'][:50])
